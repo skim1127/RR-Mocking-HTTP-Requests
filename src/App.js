d726f62be6661efffeda966b799b44faf48b0a05
Jin mo Kim <skim112790@yahoo.com>
2022-03-30 16:05:10 -0400

    finished activity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  const [gitHubName, setGitHubName] = useState('')
+  const [gitHubUrl, setGitHubUrl] = useState('')
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/skim1127')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubName(data.name)
+      setGitHubUrl(data.html_url)
+    })
+  }, [])
+
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
+      <a role="link" href={gitHubUrl}><button>Link to Github profile</button></a>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,29 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
 
+// Jest helper function
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
 
+// Test 1
+describe('Test for the get request.', () => {
+    test("Receives correct github user info using jest fetch mock", async () => {
+        fetch.mockResponseOnce(JSON.stringify({ name: 'Steven Kim'}))
+        render(<App/>)
+        const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2}))
+        expect(gitHubName).toHaveTextContent('Steven Kim')
+    })
+})
+// Test 2
+describe('Test for the Github link button', () => {
+    test("Goes to correct github user page after clicking button", async () => {
+        fetch.mockResponseOnce(JSON.stringify({ html_url: "https://github.com/users/skim1127"}))
+        render(<App/>)
+        const gitHubUrl = await waitFor(() => screen.getByRole('link'))
+        expect(gitHubUrl).toHaveAttribute('href', expect.stringContaining("https://github.com/users/skim1127"))
+    })
+})
 
